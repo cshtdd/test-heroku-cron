a26dbe09eab7fbd7c804350c2c63e13bd7736541
camilin87 <camilin87@gmail.com>
2016-09-13 18:29:28 -0400

    add a web proc just to try it out

---
+++ web.js
@@ -0,0 +1,7 @@
+var http = require('http')
+
+const PORT=process.env.PORT || 8080
+
+var server = http.createServer((request, response) => response.end("nothing to see here. move along..."))
+
+server.listen(PORT, () => console.log("Server listening on: http://localhost:%s", PORT))
